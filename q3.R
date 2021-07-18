87656d1aa53966e5a8ada3fb79991244695ae5af
oro-sean <82820387+oro-sean@users.noreply.github.com>
2021-07-19 05:27:03 +1000

    NB nested 10 fold comparision

--- q3.R
+++ q3.R
@@ -28,16 +28,18 @@ summary(rawData)
 plot_histogram(rawData)
 plot_bar(rawData)
 plot_qq(rawData)
-plot_correlation(rawData[,1:4])
+plot_correlation(rawData[ ,1:4])
 
 ## create test training split, create test data frame and create training predictors data frame and training response vector
-train_index <- createDataPartition(rawData$Auth, p=0.8, list = FALSE) # returns numerical vector of the index of the observations to be included in the training set
+train_index_10 <- createDataPartition(rawData$Auth, p=0.8, list = FALSE, times = 10) # returns numerical vector of the index of the observations to be included in the training set, repeat so we have 10 different test training sets for later
+train_index <- train_index_10[ ,1] # tuining hyperparaemters over a single test set is suitable so split out first index
 predictors <- names(rawData[-5]) # return vector of column names, removing "Auth" as it is the response variable
 
-testData <- rawData[-train_index,]
+testData <- rawData[-train_index, ]
 
 trainingPredictors <- rawData[train_index,predictors]
-trainingResponse <- as.factor(rawData[train_index,"Auth"])
+trainingResponse <- as.factor(rawData[train_index, "Auth"])
+
 
 
 ## register doParallel
@@ -59,7 +61,6 @@ bank_nb_final <- bank_nb_mod1$finalModel # save the most acurate model
 
 
 ## train and tune lda clasifier using caret
-train_control <- trainControl(method = "cv", number = 10)  # use 10 fold cross validation on the training set to asses model hyper parameters
 tune_params <- expand.grid(dimen = 0:10) # create tuning grid over available hyper parameters for NB model
 
 bank_lda_mod1 <- train(x = trainingPredictors, y = trainingResponse, method = "lda2", trControl = train_control, tuneGrid = tune_params)
@@ -75,8 +76,35 @@ conf_nb <- confusionMatrix(pred_nb$class, testData$Auth)
 pred_lda <- predict(bank_lda_mod1, newdata = testData)
 conf_lda <- confusionMatrix(pred_lda, testData$Auth)
 
+## compare models over 10 differnt test/training data sets
 
+train_control <- trainControl(method = "cv", number = 10)
+#tune_params_nb <- expand.grid(fl = bank_nb_final$tuneValue[1,1], usekernel = bank_nb_final$tuneValue[1,2], adjust = bank_nb_final$tuneValue[1,3])
+tune_params_nb <- expand.grid(usekernel = bank_nb_final$tuneValue[1,2], fL = 1, adjust = bank_nb_final$tuneValue[1,3])
+tune_params_lda <- expand.grid(dimen = bank_lda_final$tuneValue[1,1])
 
+start_time <- Sys.time()
+all_nb <- lapply(seq_len(ncol(train_index_10)), function(i){
+  
+  trainingPredictors <- rawData[train_index_10[ ,i],predictors]
+  trainingResponse <- rawData[train_index_10[ ,i],"Auth"]
+  testData <- rawData[-train_index_10[,i], ]
+  
+  bank_mod4ass_nb <- train(x = trainingPredictors, y = trainingResponse, method = "nb", trControl = train_control, tuneGrid = tune_params_nb)
+  
+  pred <- predict(bank_mod4ass_nb, newdata = testData)
+  
+  confusionMatrix(pred, testData$Auth)
+  
+  
+  
+  
+  
+})
+
+end_time <- Sys.time()
+
+(runtime <- end_time - start_time)
 ## close cluster
 stopCluster(cl)
 
